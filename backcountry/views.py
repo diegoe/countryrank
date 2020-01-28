04c9cd929c6c54199e0fe18b2496de8acf4e9962
Diego Escalante Urrelo <diegoe@gmail.com>
2020-01-28 01:31:36 -0500

    bc/views: Add /display_data/ endpoint
    Implement a bare bones /display_data/ endpoint that returns JSON
    responses to different GET parameters.
    
    Supported GET parameters are: country, indicator, year. Parameters can
    be combined for more specific sets of results.
    
    A followup to this view should be able to handle ranges and lists in the
    GET parameters so you can query for "x, y countries since a to b years
    and m and n indicators". This should become more obvious and be inspired
    by the needs of the frontend.
    
    For the purposes of this demo, the default behavior is to return the
    contents of the whole database.

--- backcountry/views.py
+++ backcountry/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from backcountry.models import CountryYearIndicator, Country, Indicator
 
 def index(request):
     return HttpResponse('''
@@ -7,6 +9,39 @@ def index(request):
         ''')
 
 def display_data(request):
-    return HttpResponse('''
-        * GET: Retrieves data stored in the database.
-        * PATCH: Updates fields in your data and save the changes to your datastore.''')
+    if request.method == 'GET':
+        q_country = request.GET.get('country', None)
+        q_indicator = request.GET.get('indicator', None)
+        q_year = request.GET.get('year', None)
+
+        r_cyi = CountryYearIndicator.objects.all()
+        r_countries = Country.objects.all()
+        r_indicators = Indicator.objects.all()
+
+        if q_country is not None:
+            r_countries = Country.objects.filter(code=q_country)
+            if len(r_countries):
+                r_cyi = r_cyi.filter(country=r_countries[0])
+
+        if q_indicator is not None:
+            r_indicators = Indicator.objects.filter(code=q_indicator)
+            if len(r_indicators):
+                r_cyi = r_cyi.filter(indicator=r_indicators[0])
+
+        if q_year is not None and q_year.isdigit():
+            r_cyi = r_cyi.filter(year=q_year)
+
+        r_cyi = list(r_cyi.values())
+        r_countries = list(r_countries.values())
+        r_indicators = list(r_indicators.values())
+
+        json = {
+                'CountryYearIndicators' : r_cyi,
+                'Countries' : r_countries,
+                'Indicators' : r_indicators,
+                }
+
+    elif request.method == 'POST':
+        json = { 'data' : None }
+
+    return JsonResponse(json)
