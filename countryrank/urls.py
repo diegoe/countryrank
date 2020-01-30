572c90b9e3393d769f1f68b63e04732ecbc4e9cc
Diego Escalante Urrelo <diegoe@gmail.com>
2020-01-30 01:27:44 -0500

    cr/urls: Install backcountry urls

--- countryrank/urls.py
+++ countryrank/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import backcountry
 
 urlpatterns = [
+    path('backcountry/', include('backcountry.urls')),
+    path('display_data/', backcountry.views.display_data),
     path('admin/', admin.site.urls),
 ]
